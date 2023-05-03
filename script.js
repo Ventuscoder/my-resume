fe3efc29c40efb809968bcea04ffc086f379a10a
Shivank Mitra <shivankcode@gmail.com>
2023-05-03 16:36:30 +0530

    Add toolset section as grid

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const loadingText = document.querySelector('.loading-text')
-const textToAdd = 'a full-stack developer'
+const textToAdd = '...a full-stack developer'
 let addedText = ''
 
 let currentTime = 0
 let blinkInterval = setInterval(()=>{
     addedText += textToAdd[addedText.length]
-    loadingText.textContent = '...' + addedText + '|'
+    loadingText.textContent = addedText + '|'
 
     if (addedText == textToAdd) {
         clearInterval(blinkInterval)
